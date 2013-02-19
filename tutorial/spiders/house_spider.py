0d3189d2a85390cfc9914c4ebb0d3270a5bbbb43
jacky <jacky@jacky.(none)>
2013-02-19 16:03:57 +0800

    districts

--- tutorial/spiders/house_spider.py
+++ tutorial/spiders/house_spider.py
@@ -1,17 +1,33 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
-class DmozSpider(BaseSpider):
-    name = "soufun"
+class HouseSpider(BaseSpider):
+    name = "house"
     allowed_domains = ["soufun.com"]
     start_urls = [
-        "http://newhouse.sh.soufun.com/house/web/Search_Result.php"
+        #"http://newhouse.sh.soufun.com/house/web/Search_Result.php"
+        "http://newhouse.sh.soufun.com/house/%C9%CF%BA%A3______%D7%A1%D5%AC__________1_1_.htm"
     ]
 
     def extract(self, select):
         return unicode.encode(select.extract()[0], 'utf-8')
 
-    def parse(self, response):
+    def parseDistricts(self, response):
+        filename = "newhouse-districts.htm"
+        out = open(filename, 'wb')
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@class="s3"]/a')
+        total = 0
+        for site in sites:
+          name = self.extract(site.select('text()'))
+          link = self.extract(site.select('@href'))
+          print name, link
+          out.write(" ".join((name, link, "</br>")))
+          total += 1
+          if total==18:
+            break
+
+    def parseList(self, response):
         filename = "newhouse.htm"
         out = open(filename, 'wb')
 
@@ -23,3 +39,6 @@ class DmozSpider(BaseSpider):
           price = self.extract(site.select('div[@class="anther"]/div[@class="antherBox"]/div[@class="price"]/span[@class="price_type"]/text()'))
           print name, house_type, price
           out.write(" ".join((name, house_type, price)))
+
+    def parse(self, response):
+        self.parseDistricts(response)
